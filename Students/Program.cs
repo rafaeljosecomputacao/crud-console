4781cf1b724cf2a79680fc79514f04978d3914af
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2024-01-31 02:20:02 -0300

    create-student-class

--- Students/Program.cs
+++ Students/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Students.Models;
+
+namespace Students
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your email: ");
+            string email = Console.ReadLine();
+
+            student.Name = name;
+            student.Email = email;
+
+            Console.WriteLine("Name: " + student.Name);
+            Console.WriteLine("Email: " + student.Email);
+        }
+    }
+}
